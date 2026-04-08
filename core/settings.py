fd6bdc0a683516aa09d44611ce949b3da3204c6a
johnalexf <johnalexfr@gmail.com>
2026-04-08 15:49:52 -0500

    chore configurar zona horaria America/Bogota y lenguaje es-co

--- core/settings.py
+++ core/settings.py
@@ -242,9 +242,9 @@ SPECTACULAR_SETTINGS = {
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-co'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Bogota'
 
 USE_I18N = True
 
